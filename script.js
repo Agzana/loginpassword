9a2eb5a025c2ba7fa4f931d964151ab670b78ac6
Agzana <grotesk_mm@list.ru>
2015-05-07 14:22:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var showpassword = function() {
-    if ($("#password").attr('type')=="password") {
-    $("#password").attr('type')="text");
+var show = function () {
+	$("#password").attr('type', 'text');
+    $("#show").attr('class','glyphicon glyphicon-eye-close');
+};
+
+var hide = function () {
+	$("#password").attr('type', 'password');
+    $("#show").attr('class', 'glyphicon glyphicon-eye-open');
+}
+
+var showpassword = function () {
+    if ($("#password").attr('type')==="password") {
+    show(); 
+	setTimeout("hide()",5000);
 } else {
-    $("#password").attr('type')="password");
+    hide();
 }
 };
       
