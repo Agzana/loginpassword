f5b810085a7f6c97dc4277acae8c4aeb32af8c59
Agzana <grotesk_mm@list.ru>
2015-05-17 21:52:52 +0300

    add

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var show = function () {
+	$("#password").attr('type', 'text');
+    $("#show").attr('class','glyphicon glyphicon-eye-close');
+};
+
+var hide = function () {
+	$("#password").attr('type', 'password');
+    $("#show").attr('class', 'glyphicon glyphicon-eye-open');
+}
+
+var showpassword = function () {
+    if ($("#password").attr('type')==="password") {
+    show(); 
+	setTimeout("hide()",5000);
+} else {
+    hide();
+}
+};
+      
+$("#show").click(showpassword);
+      
